ba6b6cc23193f3ad1b4614743156b00ffb6e93cc
ChandrikaDasari <chandrika_dasari@srmap.edu.in>
2024-08-05 18:13:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import './App.css'
+// import { MongoClient } from 'mongodb';
+// const { MongoClient } = require('mongodb');
+// // import './App.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>Task Rabbit</p>
   );
 }
+// const uri = "mongodb+srv://<chandrika>:<chandrika>@taskrabbit.v7g5bwa.mongodb.net/?retryWrites=true&w=majority&appName=TaskRabbit";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri);
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // // Send a ping to confirm a successful connection
+//     // await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
 
 export default App;
---
+++ src/Userprofile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Userprofile = () => {
+  return (
+    <div>Profile
+        <p>Login</p>
+    </div>
+    
+  )
+}
+
+export default Userprofile
